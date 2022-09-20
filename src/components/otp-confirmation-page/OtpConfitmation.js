51680412b93b6483f3739515b755fad565781dd7
Olaniyi2988 <psimeon360@gmail.com>
2022-09-21 00:06:34 +0100

    otp

--- src/components/otp-confirmation-page/OtpConfitmation.js
+++ src/components/otp-confirmation-page/OtpConfitmation.js
@@ -21,37 +21,35 @@ const OtpConfitmation = () => {
 
       <div className="w-[100%] md:w-[50%] flex items-center justify-center text-center">
         <div className='w-[80%] md:w-[60%]'>
-          <h3 className='font-bold text-3xl text-blue-dark'>Create an Account</h3>
+          <h3 className='font-bold text-3xl text-blue-dark'>Verification Code</h3>
           
-          <form className='flex flex-col gap-4'>
-            <span className='flex flex-col'>
-            <label htmlFor="" className='font-bold text-base'>First Name</label>
-            <input className='rounded-xl border-[2px] py-1 px-2 text-lg' type="text" name="Fname" placeholder='Enter First Name here' />
+          <p className='text-base my-5'>Check your email for a four digit confirmation code to verify and complete your sign up</p>
+
+          <form className='flex flex-col gap-5'>
+            <span className='space-x-5'>
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            </span>
+            <span className='space-x-5'>
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            </span>
+            <span className='space-x-5'>
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
+            <input className='w-[50px] rounded-md border-[2px] border-blue-light py-1 px-2 text-lg' type="text" />
             </span>
 
-            <span className='flex flex-col'>
-            <label htmlFor="" className='font-bold text-base'>Last Name</label>
-            <input className='rounded-xl border-[2px] py-1 px-2 text-lg' type="text" name="Lname" placeholder='Enter Last Name here' />
+            <span className=''>
+                <button className='rounded-xl bg-blue-light py-2 font-bold text-lg text-white' type="submit">Verify</button>
+                <button className='rounded-xl bg-white border-[2px] border-blue-light py-2 font-bold text-lg text-blue-light' 
+                type="submit">Resend Code</button>
             </span>
-
-            <span className='flex flex-col'>
-            <label htmlFor="" className='font-bold text-base'>Email Address</label>
-            <input className='rounded-xl border-[2px] py-1 px-2 text-lg' type="email" name="Email" placeholder='Enter Email here' />
-            </span>
-
-            <span className='flex flex-col'>
-            <label htmlFor="" className='font-bold text-base'>Password</label>
-            <input className='rounded-xl border-[2px] py-1 px-2 text-lg' type="password" name="Password" placeholder='Enter Password here' />
-            </span>
-
-            <span className='flex flex-col'>
-            <label htmlFor="" className='font-bold text-base'>Confirm Password</label>
-            <input className='rounded-xl border-[2px] py-1 px-2 text-lg' type="password" name="Password" placeholder='Enter Password here' />
-            </span>
-
-            <button className='rounded-xl bg-blue-light py-2 font-bold text-lg text-white' type="submit">Sign Up</button>
-
-            <p className='text-center'>Have an account already? <span className='ml-4 text-blue-light cursor-pointer' href='/'>LOG IN</span></p>
           </form>
         </div>
       </div>
