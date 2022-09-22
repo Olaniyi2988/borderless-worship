ceb21a77c288ff94237d4d351af158186520ef1c
Olaniyi2988 <psimeon360@gmail.com>
2022-09-22 03:46:45 +0100

    sidebar done

--- src/components/sidebar-component/SidebarComponent.js
+++ src/components/sidebar-component/SidebarComponent.js
@@ -13,33 +13,42 @@ const SidebarComponent = () => {
                 </div>
 
 
-                <div>
+                <div className='w-[100%] flex flex-col  justify-center'>
                     <ul className='flex flex-col space-y-1'>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-house"></i></span> Home</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-house"></i></span> Home</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-user"></i></span> Users</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-user"></i></span> Users</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-comments"></i></span> Testimonial</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-comments"></i></span> Testimonial</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-person-praying"></i></span> Prayer Request</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-person-praying"></i></span> Prayer Request</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-book-bible"></i></span> Bulletin</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-book-bible"></i></span> Bulletin</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-magnifying-glass"></i></span> Discover</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-magnifying-glass"></i></span> Discover</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><i class="fa-solid fa-circle-play"></i></span> Livestream</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-circle-play"></i></span> Livestream</a>
                         </li>
-                        <li className='py-2 px-5 text-base rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
-                            <a href="/"><span className='mr-5'><ion-icon name="home"></ion-icon></span> Give</a>
+                        <li className='py-1 px-5 text-lg rounded-full text-blue-dark hover:bg-blue-dark hover:text-white'>
+                            <a href="/"><span className='mr-3'><ion-icon name="home"></ion-icon></span> Give</a>
                         </li>
                     </ul>
+
+                </div>
+
+                <div className='w-[100%] flex flex-col justify-center mt-52'>
+                    <div className='flex flex-col'>
+                        <p className='py-1 px-5 text-lg rounded-full text-red  '>
+                            <a href="/"><span className='mr-3'><i class="fa-solid fa-right-from-bracket"></i></span> Logout</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
