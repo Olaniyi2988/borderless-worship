bf85698df7deca045802ccb7eaa12b6b2ce5976a
Olaniyi2988 <psimeon360@gmail.com>
2022-09-24 11:16:50 +0100

    reg

--- src/components/registration-option-page/RegistrationOption.js
+++ src/components/registration-option-page/RegistrationOption.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import regOptImg from '../../images/reg-option-img.png'
+
+
+
 
 const RegistrationOption = () => {
+    
+
+    const bgImg = {
+        backgroundImage: `url("${regOptImg}")`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat"
+    }
+
+
+    
   return (
-    <div>RegistrationOption</div>
+
+    <div className=" box-border">
+        <div className="w-3/5 h-full" style={bgImg}></div>
+        <div className="w-2/5"></div>
+    </div>
+   
   )
 }
 
